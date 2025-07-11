6e713db021ed8182bc475b476630af68a41bf3e3
alberto.nisi <alberto.nisi@edu-its.it>
2025-07-11 12:32:41 +0200

    Feature TicketBought: created TicketBoughtAdapter modified TicketBoughtActivity, TicketDetailModel and ticket_recycler_view_not_working.xml

--- app/src/main/java/com/example/infinito/ui/ticketBought/TicketBoughtActivity.kt
+++ app/src/main/java/com/example/infinito/ui/ticketBought/TicketBoughtActivity.kt
@@ -5,17 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 import com.example.infinito.R
+import com.example.infinito.data.model.TicketDetailModel
 
 class TicketBoughtActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_ticket_bought)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        var numberOfTickets = 1
+        val ticketBoughtArray = ArrayList<TicketDetailModel>()
+        val ticketBought = TicketDetailModel("Serata Osservativa","25/07/2025", "14:30","Intera", 14.00)
+
     }
+
 }
