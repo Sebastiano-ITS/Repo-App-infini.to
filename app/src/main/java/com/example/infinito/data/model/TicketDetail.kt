db1c2b8826bd56f5c4a2b604e2af717c6c83129c
alberto.nisi <alberto.nisi@edu-its.it>
2025-07-11 10:32:43 +0200

    created the model for the ticket bought

---
+++ app/src/main/java/com/example/infinito/data/model/TicketDetail.kt
@@ -0,0 +1,9 @@
+package com.example.infinito.data.model
+
+data class TicketDetail (
+    val eventTitle: String,
+    val ticketDate: String,
+    val ticketTime: String,
+    val ticketType: String,
+    val ticketPrice: Double
+)
